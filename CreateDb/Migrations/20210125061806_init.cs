18c367fd4b2a20658ddcee2ee595143118ef3031
nikitarasevskij <squidrash777@gmail.com>
2021-01-25 14:53:44 +0300

    2) добавил класс с методами для меню

---
+++ CreateDb/Migrations/20210125061806_init.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CreateDb.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductName = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(nullable: false),
+                    Discount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserEntityId = table.Column<int>(nullable: false),
+                    CreatTime = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_UserEntityId",
+                        column: x => x.UserEntityId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bascets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderEntityId = table.Column<int>(nullable: false),
+                    MenuEntityId = table.Column<int>(nullable: false),
+                    CountDish = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bascets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bascets_Menus_MenuEntityId",
+                        column: x => x.MenuEntityId,
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bascets_Orders_OrderEntityId",
+                        column: x => x.OrderEntityId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bascets_MenuEntityId",
+                table: "Bascets",
+                column: "MenuEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bascets_OrderEntityId",
+                table: "Bascets",
+                column: "OrderEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserEntityId",
+                table: "Orders",
+                column: "UserEntityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bascets");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ CreateDb/Services/MenuService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using CreateDb.Storage;
+using CreateDb.Storage.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CreateDb.Services
+{
+    public interface IMenuService
+    {
+        public System.Collections.Generic.List<Storage.Models.MenuEntity> GetMenu();
+
+        public void AddToMenu(MenuEntity[] menuEntity);
+
+        public void EditMenu();
+        
+    }
+    public class MenuService : IMenuService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        public MenuService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public System.Collections.Generic.List<Storage.Models.MenuEntity> GetMenu()
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var _context = scope.ServiceProvider.GetRequiredService<PizzaDbContext>();
+
+            var fullMenu = _context.Menus.ToList();
+            foreach(var m in fullMenu)
+            {
+                Console.WriteLine($"{m.ProductName} - {m.Price}");
+            }
+            return fullMenu;
+        }
+        public void AddToMenu(MenuEntity[] menuEntity)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var _context = scope.ServiceProvider.GetRequiredService<PizzaDbContext>();
+
+            foreach (var m in menuEntity)
+            {
+                _context.Menus.Add(m);
+            }
+            _context.SaveChanges();
+        }
+        public void EditMenu()
+        {
+
+        }
+    }
+}
